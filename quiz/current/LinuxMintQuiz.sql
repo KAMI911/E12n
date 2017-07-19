f3fe93d08254cd5652ef688ea5f4277e5433373e
Kálmán Szalai - KAMI <kami911@gmail.com>
2017-07-19 13:55:33 +0200

    Linux Mint kvíz kezdemény hozzáadása

---
+++ quiz/current/LinuxMintQuiz.sql
@@ -0,0 +1,61 @@
+
+INSERT INTO `mq_new`.`quizQuestions` (`idQuestion`,`textQuestion`,`quizLanguage`,`quizType`) VALUES (51,"Milyen változatokban érhető el a Linux Mint?","hu",2);
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (51,1,"Cinnamon","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (51,2,"Xfce","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (51,3,"MATE","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (51,4,"KDE","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (51,4,"Debian Edition","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (51,4,"Gamers' Edition","hu");
+INSERT INTO `mq_new`.`rightAnswers` (`idQuestion`,`idChoice`,`score`) VALUES (51,1,5);
+INSERT INTO `mq_new`.`rightAnswers` (`idQuestion`,`idChoice`,`score`) VALUES (51,2,5);
+INSERT INTO `mq_new`.`rightAnswers` (`idQuestion`,`idChoice`,`score`) VALUES (51,3,5);
+INSERT INTO `mq_new`.`rightAnswers` (`idQuestion`,`idChoice`,`score`) VALUES (51,4,5);
+INSERT INTO `mq_new`.`rightAnswers` (`idQuestion`,`idChoice`,`score`) VALUES (51,5,5);
+
+INSERT INTO `mq_new`.`quizQuestions` (`idQuestion`,`textQuestion`,`quizLanguage`,`quizType`) VALUES (52,"Melyek a Linux Mint „felmenői”?","hu",2);
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (52,1,"Fedora","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (52,2,"Debian","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (52,3,"Ubuntu","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (52,4,"Slackware","hu");
+INSERT INTO `mq_new`.`rightAnswers` (`idQuestion`,`idChoice`,`score`) VALUES (52,2,5);
+INSERT INTO `mq_new`.`rightAnswers` (`idQuestion`,`idChoice`,`score`) VALUES (52,3,5);
+
+INSERT INTO `mq_new`.`quizQuestions` (`idQuestion`,`textQuestion`,`quizLanguage`,`quizType`) VALUES (53,"Mi alapján nevezik el a különféle Linux Mint verziókat?","hu",2);
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (53,1,"Bolygók neveinek alapján.","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (53,2,"Női neveknek megfelelően.","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (53,3,"Egzotikus üdülőhelyek nevei szerint.","hu");
+INSERT INTO `mq_new`.`rightAnswers` (`idQuestion`,`idChoice`,`score`) VALUES (53,2,5);
+
+INSERT INTO `mq_new`.`quizQuestions` (`idQuestion`,`textQuestion`,`quizLanguage`,`quizType`) VALUES (54,"Mi az a kernel?","hu",2);
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (54,1,"Kern András nemrég megjelent, melankolikus hangulatú nagylemeze.","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (54,2,"Az operációs rendszer alapja, amely felelős a hardver erőforrásainak kezeléséért.","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (54,3,"Friss űrtechnikai fejlesztés, amely a billentyűzetet fogja leváltani hamarosan.","hu");
+INSERT INTO `mq_new`.`rightAnswers` (`idQuestion`,`idChoice`,`score`) VALUES (55,2,5);
+
+INSERT INTO `mq_new`.`quizQuestions` (`idQuestion`,`textQuestion`,`quizLanguage`,`quizType`) VALUES (55,"A Linux Mint, az Ubuntu, a Fedora operációs rendszer:","hu",2);
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (55,1,"Ingyenesen letölthető és használható, a frissítések is ingyenesek.","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (55,2,"Fizetés ellenében letölthető és használható, a frissítések viszont ingyenesek.","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (55,3,"fizetés ellenében letölthető és használható, és a frissítések is.","hu");
+INSERT INTO `mq_new`.`rightAnswers` (`idQuestion`,`idChoice`,`score`) VALUES (55,1,5);
+
+INSERT INTO `mq_new`.`quizQuestions` (`idQuestion`,`textQuestion`,`quizLanguage`,`quizType`) VALUES (56,"Milyen programok érhetők el a Linux Mint operációs rendszerben?","hu",2);
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (56,1,"Firefox böngésző.","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (56,2,"LibreOffice irodai programcsomag.","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (56,3,"VLC videólejátszó, GThumb képmegjelenítő.","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (56,4,"Pidgin azonnali üzenetküldő.","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (56,5,"Transmission torrent kliens.","hu");
+INSERT INTO `mq_new`.`quizQuestionChoices` (`idQuestion`,`idChoice`,`textChoice`,`quizLanguage`) VALUES (56,6,"Több ezer egyéb média-, irodai-, tudományos- és játékprogram.","hu");
+INSERT INTO `mq_new`.`rightAnswers` (`idQuestion`,`idChoice`,`score`) VALUES (56,1,5);
+INSERT INTO `mq_new`.`rightAnswers` (`idQuestion`,`idChoice`,`score`) VALUES (56,2,5);
+INSERT INTO `mq_new`.`rightAnswers` (`idQuestion`,`idChoice`,`score`) VALUES (56,3,5);
+INSERT INTO `mq_new`.`rightAnswers` (`idQuestion`,`idChoice`,`score`) VALUES (56,4,5);
+INSERT INTO `mq_new`.`rightAnswers` (`idQuestion`,`idChoice`,`score`) VALUES (56,5,5);
+INSERT INTO `mq_new`.`rightAnswers` (`idQuestion`,`idChoice`,`score`) VALUES (56,6,5);
+
+INSERT INTO `mq_new`.`quizQuestions` (`idQuestion`,`textQuestion`,`quizLanguage`,`quizType`) VALUES (5,"Mi a kabalaállata a Linuxnak?,"hu",2);
+
+INSERT INTO `mq_new`.`quizQuestions` (`idQuestion`,`textQuestion`,`quizLanguage`,`quizType`) VALUES (6,"Milyen tulajdonsággal rendelkeznek a szabad szoftverek?","hu",2);
+
+
+
+
